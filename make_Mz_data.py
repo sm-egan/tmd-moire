Fixed the finite T formula for edge orbital magnetization

make_Mz_data.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
             evals, evecs = eigsh(Hplus, k=100, which='SM') # 'SM' means take smallest magnitude eigenvalues
             #print('Eigenvalue problem time: ' + str(time.process_time() - start))             
             
-            Bc = Berry_curv(k, mu, band, evals, evecs)
+            Bc = Berry_curv(k, band, evals, evecs)
             try:
                 Bc_array = np.vstack((Bc_array, Bc))
             except:

tTMDpresets.py
@@ -29,7 +29,7 @@ meff = 0.5*m_e #kg
 
 muB = physical_constants['Bohr magneton'][0]
 mu0 = physical_constants['vacuum mag. permeability'][0]
-# Converts m(q) units from A Angstrom^2 to muB. 1e-20 converts from Angstrom^2 to m^2
+# Converts m(k) units from A Angstrom^2 to muB. 1e-20 converts from Angstrom^2 to m^2
 AAng_to_muB = 1e-20/muB
 
 Gj = 4*pi/(np.sqrt(3)*a) # 1/Angstrom
@@ -379,8 +379,6 @@ def Berry_curv(k0, nindex, evals, evecs, xi=1):
     ----------
     k0 : kMvec
         k point at which to evaluate.
-    mu : float
-        chemical potential.
     nindex : int
         band index.
     evals : ndarray
@@ -430,8 +428,8 @@ def Berry_curv(k0, nindex, evals, evecs, xi=1):
         Bc_tot += Bc_coeff*(dxdy - dydx)
         m_tot += m_coeff*(dxdy - dydx)
         
-    # Returns in units of Ampere*Angstrom^2
-    return np.imag(np.array([-Bc_tot, m_tot]))
+    # Bc_tot in units of Ang^2, m_tot in units of muB
+    return np.imag(np.array([-Bc_tot, AAng_to_muB*m_tot]))
 
 class BerryCurvQuantities:
     def __init__(self, Bc_array, band, mu, temp=0):
@@ -500,26 +498,68 @@ class BerryCurvQuantities:
         m_list = m_list*fe
         # Units: muB/nm^2
         if m_list.ndim == 1:
-            return np.sum(m_list)/(len(evals)*Auc_nm)
+            return np.sum(m_list)/len(evals)
         elif m_list.ndim == 2:
-            print('M_list is 2 dimensional')
-            return np.sum(m_list, axis = 1)/(len(evals)*Auc_nm)
-    
-    def get_Mz_edge(self, evals):
-        bc_list = self._Bc_array[:, 0]
-        fe = nFD(evals, self.mus, self.temp)
+            #print('M_list is 2 dimensional')
+            return np.sum(m_list, axis = 1)/len(evals)
         
+    def get_mk_edge(self, evals):
+        '''
+
+        Parameters
+        ----------
+        evals : ndarray
+            Array of eigenvalues enk for band n.
+
+        Raises
+        ------
+        Exception
+            Terminates calculation if the argument is too large to be computed.
+
+        Returns
+        -------
+        ndarray
+            array of mk,edge in units of muB.  May be either ndim = 1 (single mu) or ndim = 2 (list of mu)
+
+        '''
+        bc_list = self._Bc_array[:, 0] #Extract Berry curvature \Omega from Bc_array
+        
+        # If there is a list of mus use broadcasting to compute the pre-factor
         if isinstance(self.mus, np.ndarray):
-            mus_rs = self.mus.reshape(self.mus.shape + (1,))
-            medge_list = (mus_rs - evals)*bc_list
+            mu_list = self.mus.reshape(self.mus.shape + (1,))
+        else: #else if computing for single mu
+            mu_list = self.mus
+        #     medge_list = (mus_rs - evals)*bc_list
+        # else:
+        #     medge_list = (self.mus - evals)*bc_list
+        if self.temp == 0:
+            medge_list = e/hbar*(eV/1000)*(mu_list - evals)*nFD(evals, self.mus, self.temp)*bc_list
         else:
-            medge_list = (self.mus - evals)*bc_list
-        medge_list = e/hbar*(eV/1000)*AAng_to_muB*medge_list*fe
+            x = -(evals - mu_list)/(kB*self.temp/(eV/1000))
+            if np.any(x > 7e2):
+                raise Exception('np.exp cannot compute such large positive arguments')
+            medge_list = e/hbar*kB*self.temp*np.log(1 + np.exp(x))*bc_list
+                
+        return medge_list*AAng_to_muB
+    
+    def get_Mz_edge(self, medge_list):
+        '''
         
+        Parameters
+        ----------
+        medge_list : ndarray
+            Array of mk,edge computed using get_mk_edge in units of muB.
+
+        Returns
+        -------
+        float
+            DESCRIPTION.
+
+        '''
         if medge_list.ndim == 1:
-            return np.sum(medge_list)/(len(evals)*Auc_nm)
+            return np.sum(medge_list)/len(self._Bc_array)
         elif medge_list.ndim == 2:
-            return np.sum(medge_list, axis = 1)/(len(evals)*Auc_nm)
+            return np.sum(medge_list, axis = 1)/len(self._Bc_array)
         else:
             print('Invalid number of dimensions')
         

vs_mu_plotting.py
@@ -43,7 +43,8 @@ if __name__ == '__main__':
             bcq = BerryCurvQuantities(Bc_array, band, mu_list, T)
             Bc = bcq.get_Bc(evals_array[:,band])
             Mz_int = bcq.get_Mz_int(evals_array[:,band])
-            Mz_edge = bcq.get_Mz_edge(evals_array[:,band])
+            mk_edge = bcq.get_mk_edge(evals_array[:,band])
+            Mz_edge = bcq.get_Mz_edge(mk_edge)
             susc = bcq.get_orb_susc(evals_array[:,band], True)
             if band == 0:
                 mu_mask = (mu_list > band_bottom[band] - bandwidth[band]) & (mu_list < band_top[band] + 2*bandwidth[band])
@@ -56,7 +57,12 @@ if __name__ == '__main__':
             #     title = 'Band {}'.format(band + 1) + ', C = ' + chern_band[band]
             #     plot_grid(fbz_mesh, AAng_to_muB*Bc_array[:,0], grid='FBZ', title = title, cvalue='Berrycurv')
             #     plot_grid(fbz_mesh, AAng_to_muB*Bc_array[:,1], grid='FBZ', title = title)
-                
+            
+            if band == 1:
+                ind_band1 = np.argmin(np.abs(mu_list-band_top[1]))
+                plot_grid(fbz_mesh, mk_edge[ind_band1], grid='FBZ', title = title)
+                Mz_edge_band1 = Mz_edge
+            
             if band == 0:
                 Bc_arr = Bc
                 Mz_int_arr = Mz_int
@@ -126,5 +132,3 @@ if __name__ == '__main__':
     plt.grid(axis='both', which='both')
     plt.show()
 
-    
-    